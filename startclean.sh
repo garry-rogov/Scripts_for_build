021d0f6901a5af95bcd996550c05075c16f66854
garry-rogov <garry.rogov@gmail.com>
2022-08-27 07:00:47 +0300

    update startclean

--- startclean.sh
+++ startclean.sh
@@ -3,10 +3,5 @@
 rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/display
 rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/media
 rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/audio
-rm -rf ~/source/android/crDroid12.1/vendor/qcom/opensource/interfaces
-rm -rf ~/source/android/crDroid12.1/vendor/nxp/opensource/commonsys/external/libnfc-nci
-rm -rf ~/source/android/crDroid12.1/vendor/nxp/opensource/commonsys/frameworks
-rm -rf ~/source/android/crDroid12.1/vendor/nxp/opensource/interfaces/nfc
-rm -rf ~/source/android/crDroid12.1/vendor/nxp/opensource/sn100x/halimpl
-rm -rf ~/source/android/crDroid12.1/vendor/nxp/opensource/sn100x/hidlimpl
+
 
