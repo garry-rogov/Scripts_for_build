028f84c6e44f8a108cd6d13ec778b7bb392a9c0c
garry-rogov <garry.rogov@gmail.com>
2022-09-01 05:13:10 +0300

    update startclean

--- startclean.sh
+++ startclean.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/display
-rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/media
-rm -rf ~/source/android/crDroid12.1/hardware/qcom-caf/sm8150/audio
-
 
